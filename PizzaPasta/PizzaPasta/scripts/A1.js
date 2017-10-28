ee7be243bf61cb1df2d54e6e1c2abb19e3566fb5
landenf <landenfogle@gmail.com>
2017-10-28 09:31:54 -0500

    Started to work making new branch to continue

--- PizzaPasta/PizzaPasta/scripts/A1.js
+++ PizzaPasta/PizzaPasta/scripts/A1.js
@@ -1,118 +1,58 @@
 //Typscript for this project:
 //Varibles------------------------
-var wallet = 5.00;
+var Total = 0.00;
 var AdddressFinal = "";
 //functions----------------------
 function buyMeatBalls() {
     // let someValue = 6;
-    if (wallet >= 2) {
-        wallet = wallet - 2;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 2;
+    document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyextracheese() {
     // let someValue = 6;
-    if (wallet >= .5) {
-        wallet = wallet - .5;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + .5;
+    document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyextraBasil() {
     // let someValue = 6;
-    if (wallet >= 1.00) {
-        wallet = wallet - 1.00;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 1.00;
+    document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyTomatoSauce() {
     // let someValue = 6;
-    if (wallet >= 3.50) {
-        wallet = wallet - 3.50;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 3.50;
+    document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyAlfredoSauce() {
     // let someValue = 6;
-    if (wallet >= 5) {
-        wallet = wallet - 5;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 5;
+    document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 //.
 function buyMeatBalls1() {
     // let someValue = 6;
-    if (wallet >= 2) {
-        wallet = wallet - 2;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 2;
+    document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyextracheese1() {
     // let someValue = 6;
-    if (wallet >= .5) {
-        wallet = wallet - .5;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + .5;
+    document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyextraSauce1() {
     // let someValue = 6;
-    if (wallet >= 1.00) {
-        wallet = wallet - 1.00;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 1.00;
+    document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyTomatoSauce1() {
     // let someValue = 6;
-    if (wallet >= 3.50) {
-        wallet = wallet - 3.50;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 3.50;
+    document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function buyAlfredoSauce1() {
     // let someValue = 6;
-    if (wallet >= 5) {
-        wallet = wallet - 5;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+    Total = Total + 5;
+    document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 function addressf(address) {
     alert("Thank you for ordering with us! Your pizza/pasta will be deliverd to you at: " + address + "  It should be there soon!  Please be ready to pay when it gets there. Thank you for doing buissness with Online Itailan Restrant.");
--- PizzaPasta/PizzaPasta/scripts/A1.ts
+++ PizzaPasta/PizzaPasta/scripts/A1.ts
@@ -1,123 +1,68 @@
 ﻿//Typscript for this project:
 //Varibles------------------------
-var wallet = 5.00;
+var Total = 0.00;
 var AdddressFinal = "";
 //functions----------------------
 function buyMeatBalls() {
     // let someValue = 6;
-    if (wallet >= 2) {
-        wallet = wallet - 2;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
+        Total = Total + 2;
+        document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
 }
 function buyextracheese() {
     // let someValue = 6;
-    if (wallet >= .5) {
-        wallet = wallet - .5;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
+        Total = Total + .5;
+        document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
 }
 function buyextraBasil() {
     // let someValue = 6;
-    if (wallet >= 1.00) {
-        wallet = wallet - 1.00;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
+        Total = Total + 1.00;
+        document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
 
     }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-}
+
 function buyTomatoSauce() {
     // let someValue = 6;
-    if (wallet >= 3.50) {
-        wallet = wallet - 3.50;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+        Total = Total + 3.50;
+        document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
+
 }
 function buyAlfredoSauce() {
     // let someValue = 6;
-    if (wallet >= 5) {
-        wallet = wallet - 5;
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+        Total = Total + 5;
+        document.getElementById("MoneyLeft").innerHTML = "$" + Total.toString() + " Money Spent";
+
 }
 //.
 function buyMeatBalls1() {
     // let someValue = 6;
-    if (wallet >= 2) {
-        wallet = wallet - 2;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+        Total = Total + 2;
+        document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
+
 }
 function buyextracheese1() {
     // let someValue = 6;
-    if (wallet >= .5) {
-        wallet = wallet - .5;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
+        Total = Total + .5;
+        document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
 }
 function buyextraSauce1() {
     // let someValue = 6;
-    if (wallet >= 1.00) {
-        wallet = wallet - 1.00;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
+        Total = Total + 1.00;
+        document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
 }
 function buyTomatoSauce1() {
     // let someValue = 6;
-    if (wallet >= 3.50) {
-        wallet = wallet - 3.50;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+        Total = Total + 3.50;
+        document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
+
 }
 function buyAlfredoSauce1() {
     // let someValue = 6;
-    if (wallet >= 5) {
-        wallet = wallet - 5;
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
-    else {
-        alert("Your broke!");
-        document.getElementById("MoneyLeft1").innerHTML = "$" + wallet.toString() + " Left to spend";
-    }
+        Total = Total + 5;
+        document.getElementById("MoneyLeft1").innerHTML = "$" + Total.toString() + " Money Spent";
 }
 
 function addressf(address) {
